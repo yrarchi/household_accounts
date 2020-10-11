d2f4d44ecc607e595cea8955794f99cc496641f7
yrarchi <37015480+yrarchi@users.noreply.github.com>
2020-10-11 12:35:05 +0900

    ファイル名の修正

--- household_accounts/__main__.py
+++ household_accounts/__main__.py
@@ -1,9 +1,9 @@
 import ocr
-import cut_out_images
+import cut_out_receipts
 from gui_make_pages import MakeGuiScreen
 from gui_show_receipt_contours import MakeFirstPage
 
-cut_out_images.main()
+cut_out_receipts.main()
 ocr_results = ocr.main()
 gui = MakeGuiScreen(ocr_results)
 MakeFirstPage(gui)
