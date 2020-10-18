95d411e4b9ae4ebf26cbfd25e52ed777abefc89f
yrarchi <37015480+yrarchi@users.noreply.github.com>
2020-10-18 13:00:46 +0900

    金額列も割引列と同等の処理にする

--- household_accounts/calc.py
+++ household_accounts/calc.py
@@ -5,17 +5,11 @@ def calc_price_tax_in(price_list, discount_list, reduced_tax_rate_flg_list, tax_
     tax_rate = config.tax_rate
     reduced_tax_rate = config.reduced_tax_rate
     
-    trans_price_list = []
-    for price in price_list:
-        try:
-            price = int(price)
-        except ValueError:  # 金額を数値として読み取れていない（アルファベット等として認識）場合はいったん0円とする
-            price = 0
-        trans_price_list.append(price)
-    
+    # 金額を数値として読み取れていない場合はいったん0円とする
+    price_list = [int(x) if re.fullmatch(r'-*[0-9]+', str(x)) else 0 for x in price_list]
     discount_list = [int(x) if re.fullmatch(r'-*[0-9]+', str(x)) else 0 for x in discount_list]
-    price_discount_list = [p+d for p, d in zip(trans_price_list, discount_list)]
-
+    
+    price_discount_list = [p+d for p, d in zip(price_list, discount_list)]
     price_tax_in_list = []
     if tax_excluded_flg:
         for row, (price, reduced_tax_rate_flg) in enumerate(zip(price_discount_list, reduced_tax_rate_flg_list)):
@@ -23,7 +17,7 @@ def calc_price_tax_in(price_list, discount_list, reduced_tax_rate_flg_list, tax_
             tax = reduced_tax_rate if reduced_tax_rate_flg else tax_rate
             price_tax_in_list.append(int(price * tax))  # 税込にして端数が出た場合は切り捨てとして扱う
     else:
-        price_tax_in_list = trans_price_list
+        price_tax_in_list = price_discount_list
     return price_tax_in_list
 
 
