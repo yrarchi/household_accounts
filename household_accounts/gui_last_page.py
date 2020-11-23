092e890b6de07d6b9f8c4569b03c549e8caaf876
yrarchi <37015480+yrarchi@users.noreply.github.com>
2020-11-23 11:50:53 +0900

    GUI閉じるときに中間ファイルを消す

--- household_accounts/gui_last_page.py
+++ household_accounts/gui_last_page.py
@@ -1,12 +1,28 @@
 import datetime
+import glob
+import os
 import tkinter as tk
 
 def show_last_page(gui):
+    def show_message():
+        today = datetime.datetime.now().strftime('%Y%m%d')
+        csv_path = 'csv/{}.csv'.format(today)
+        message = '終了しました。読み取ったデータは{}に保存されています。'.format(csv_path)
+        message_label = tk.Label(gui.next_page, text=message)
+        message_label.pack(anchor='center')
+    
+    def show_close_button():
+        def close_button_function():
+            img_delete_path = os.path.join(os.path.dirname(__file__), '../img/interim/**/*.png')
+            img_delete_path_list = glob.glob(img_delete_path, recursive=True)
+            for p in img_delete_path_list:
+                if os.path.isfile(p):
+                    os.remove(p)
+            gui.destroy()
+    
+        close_button = tk.Button(gui.next_page, text='閉じる', command=close_button_function)
+        close_button.pack(anchor='s', ipadx=100, ipady=15)
+
     gui.change_page()
-    today = datetime.datetime.now().strftime('%Y%m%d')
-    csv_path = 'csv/{}.csv'.format(today)
-    message = '終了しました。読み取ったデータは{}に保存されています。'.format(csv_path)
-    message_label = tk.Label(gui.next_page, text=message)
-    message_label.pack(anchor='center')
-    close_button = tk.Button(gui.next_page, text='閉じる', command=gui.destroy)
-    close_button.pack(anchor='s', ipadx=100, ipady=15)
+    show_message()
+    show_close_button()
